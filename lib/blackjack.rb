b5740c70c84d8c31445bd83c96e41c7a2170d5bf
Hailey Ringier <haileyringier@outlook.com>
2020-06-21 20:21:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,7 +37,7 @@ def hit? (card_total)
     new_card = deal_card
     card_total = card_total + new_card
   end
-  if player_answer != "h" | "s"
+  if player_answer != "h" || "s"
     invalid_command
     prompt_user
   end
